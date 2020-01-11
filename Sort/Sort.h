cfc754c829566f02c6f3e8b7f063b50cb5f3c910
MMTsallagova <55704942+MMTsallagova@users.noreply.github.com>
2020-01-12 02:23:14 +0300

    Lab1

---
+++ Sort/Sort.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+
+const int BORDER = 15;
+
+template<typename T>
+void swap(T* a, T* b) {
+	T tmp = *a;
+	*a = *b;
+	*b = tmp;
+	return;
+}
+
+template <typename T, typename Compare>
+void sort(T *first, T *last, Compare comp) {
+	int length = last - first + 1;
+
+	if (length < 2) return;
+
+	if (length < BORDER)
+		insertsort(first, last, comp);
+	else
+		quicksort(first, last, comp);
+}
+
+
+template <typename T, typename Compare>
+void insertsort(T *first, T *last, Compare comp) {
+	int length = last - first + 1;
+	for (int i = 1; i < length; i++)
+		for (int j = i; j > 0 && !comp(*(first + j - 1), *(first + j)); j--)
+			swap(first + j - 1, first + j);
+};
+
+template <typename T, typename Compare>
+T pivot_point(T *first, T *last, Compare comp) {
+	T tmp_array[3];
+	T* middle = first + (last - first) / 2;
+	tmp_array[0] = *first;
+	tmp_array[1] = *middle;
+	tmp_array[2] = *last;
+	insertsort(tmp_array, tmp_array + (std::size(tmp_array) - 1), comp);
+	return tmp_array[1];
+}
+
+template <typename T, typename Compare>
+void quicksort(T *first, T *last, Compare comp) {
+	int length = last - first + 1;
+
+	T pivot = pivot_point(first, last, comp);
+
+	int left = 0;
+	int right = length - 1;
+	while (left <= right) {
+		while (comp(first[left], pivot))
+			left++;
+		while (comp(pivot, first[right]))
+			right--;
+		if (left <= right) {
+			swap(&first[left], &first[right]);
+			left++;
+			right--;
+		}
+	}
+	if (right > 0) 
+		sort(first, first + right, comp);
+	if (length > left) 
+		sort(first + left, last, comp);
+};
---
+++ UnitTest1/Tests.cpp
@@ -0,0 +1,129 @@
+﻿#include "stdafx.h"
+#include <algorithm>
+#include <random>
+#include "CppUnitTest.h"
+#include "../Sort/Sort.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLesson1
+{
+	TEST_CLASS(UnitTestSort)
+	{
+	public:
+		TEST_METHOD(TestSwap)
+		{
+			int arrayTest[2] = { 1, 2 };
+			int expectedArray[2] = {2, 1};
+
+			swap(arrayTest, arrayTest + 1);
+			for (int i = 0; i < 2; i++)
+			{
+				Assert::AreEqual(expectedArray[i], arrayTest[i]);
+			}
+		}
+
+		TEST_METHOD(PivotTest) {
+			const int length = 7;
+			int arrayTest[length] = { 6, 5, 4, 3, 2, 1, 0 };
+			int expectedArray[length] = { 0, 1, 2, 3, 4, 5, 6 };
+			auto lambda = [](int a, int b) { return a < b; };
+
+			int pivot = pivot_point(arrayTest, arrayTest + length - 1, lambda);
+			Assert::AreEqual(3, pivot);
+		}
+
+		TEST_METHOD(EmptyArrayTest)
+		{
+			const int length = 1;
+			int arrayTest[length] = {};
+			int expectedArray[length] = {};
+			auto lambda = [](int a, int b) { return a < b; };
+
+			sort(arrayTest, arrayTest + length - 1, lambda);
+			for (int i = 0; i < length; i++)
+			{
+				Assert::AreEqual(expectedArray[i], arrayTest[i]);
+			}
+		}
+		
+		TEST_METHOD(SingleElementTest)
+		{
+			int arrayTest[] = { 1 };
+			auto lambda = [](int a, int b) { return a < b; };
+			sort(arrayTest, arrayTest, lambda);
+			Assert::AreEqual(1, arrayTest[0]);
+		}
+		
+		TEST_METHOD(InsertTest_WorstCase) 
+		{
+			const int length = 7;
+			int arrayTest[length] = { 6, 5, 4, 3, 2, 1, 0 };
+			int expectedArray[length] = { 0, 1, 2, 3, 4, 5, 6 };
+			auto lambda = [](int a, int b) { return a < b; };
+
+			sort(arrayTest, arrayTest + length - 1, lambda);
+
+			for (int i = 0; i < length; i++)
+			{
+				Assert::AreEqual(expectedArray[i], arrayTest[i]);
+			}
+		}
+
+		TEST_METHOD(Insert_Float)
+		{
+			const int length = 7;
+			float arrayTest[length] = { 1.6, 0.5, 2.1, 0.2, 0.0, 4.3, 3.4 };
+			float expectedArray[length] = { 0.0, 0.2, 0.5, 1.6, 2.1, 3.4, 4.3 };
+			auto lambda = [](int a, int b) { return a < b; };
+
+			sort(arrayTest, arrayTest + length - 1, lambda);
+			for (int i = 0; i < length; i++)
+			{
+				Assert::AreEqual(expectedArray[i], arrayTest[i]);
+			}
+		}
+
+		TEST_METHOD(QuickSort) 
+		{
+			const int length = 30;
+			auto lambda = [](int a, int b) { return a < b; };
+			int arrayTest[length] = {};
+			int expectedArray[length] = {};
+
+			for (int i = 0; i < length; i++) {
+				int randInt = rand() % 100;
+				arrayTest[i] = randInt;
+				expectedArray[i] = randInt;
+			}
+			std::sort(expectedArray, expectedArray + length);
+			sort(arrayTest, arrayTest + length - 1, lambda);
+
+			for (int i = 0; i < length; i++)
+			{
+				Assert::AreEqual(expectedArray[i], arrayTest[i]);
+			}
+		}
+
+		TEST_METHOD(QuickSort_100elements)
+		{
+			const int length = 100;
+			auto lambda = [](int a, int b) { return a < b; };
+			int arrayTest[length] = {};
+			int expectedArray[length] = {};
+
+			for (int i = 0; i < length; i++) {
+				int randInt = rand() % 100;
+				arrayTest[i] = randInt;
+				expectedArray[i] = randInt;
+			}
+			std::sort(expectedArray, expectedArray + length);
+			sort(arrayTest, arrayTest + length -1, lambda);
+
+			for (int i = 0; i < length; i++)
+			{
+				Assert::AreEqual(expectedArray[i], arrayTest[i]);
+			}
+		}
+	};
+}
